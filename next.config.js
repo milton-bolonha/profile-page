4814bb2936233649c07a32a47219676ecee7eb9e
Milton Bolonha <miltonsiedler@gmail.com>
2025-12-25 11:45:34 -0300

    update/cors glb png models

--- next.config.js
+++ next.config.js
@@ -20,6 +20,31 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/games/stranger-craft/models/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, HEAD, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Range",
+          },
+          {
+            key: "Cross-Origin-Resource-Policy",
+            value: "cross-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
